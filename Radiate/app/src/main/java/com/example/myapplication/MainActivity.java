2279b45c8f2e1a0262546dbd4355f7b8a64f825e
TheAidaan <56072883+TheAidaan@users.noreply.github.com>
2021-08-31 19:24:09 +0200

    popups
    login and sign up

--- Radiate/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Radiate/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,28 +3,35 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences.Editor _sharedPrefsEdit;
     SharedPreferences _appSettingPrefs;
+
     Boolean _isNightModeOn;
+    Dialog _dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton btnLogo  = findViewById(R.id.btnlogo_welcome);
         _appSettingPrefs = getSharedPreferences("AppSettingPrefs",0); // get the storage reference
         _sharedPrefsEdit = _appSettingPrefs.edit(); //set the storage editor reference
 
+        _dialog = new Dialog(this);
 
-        //night mode for activity
+        //region night mode for activity
 
         _isNightModeOn = _appSettingPrefs.getBoolean("NightMode",false); //check the variable status
 
@@ -33,14 +40,28 @@ public class MainActivity extends AppCompatActivity {
         else
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); //day
 
-        //night mode for activity
+        //endregion
 
+        ImageButton btnLogo  = findViewById(R.id.btnlogo_welcome);
         btnLogo.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 ChangeTheme();
             }
         });
 
+        Button btnLogin  = findViewById(R.id.btnLogin_welcome);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ShowLoginPopUp();
+            }
+        });
+
+        Button btnSignUp  = findViewById(R.id.btnSignup_welcome);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ShowSignUpPopUp();
+            }
+        });
     }
 
     void ChangeTheme() {
@@ -55,6 +76,76 @@ public class MainActivity extends AppCompatActivity {
             _sharedPrefsEdit.putBoolean("NightMode",true);
         }
         _sharedPrefsEdit.apply();
-    } // called by btnLogin
+    } // called by btnLogo
 
+    void ShowLoginPopUp(){
+        _dialog.setContentView(R.layout.login_popup);
+
+        //region Cancel
+        Button btnCancel = _dialog.findViewById(R.id.btnCancel_LoginPopup);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                _dialog.dismiss();
+            }
+        });
+        //endregion
+
+        //region Login
+        Button btnDone = _dialog.findViewById(R.id.btnDone_LoginPopup);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class); //replace with check login
+                startActivity(intent);
+            }
+        });
+        //endregion
+
+        //region google auth
+        Button btnGoogle = _dialog.findViewById(R.id.btnLoginGoogle_LoginPopup);
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class); //replace with check login
+                startActivity(intent);
+            }
+        });
+        //endregion
+
+        _dialog.show();
+    }//called by btnLogin
+
+    void ShowSignUpPopUp(){
+        _dialog.setContentView(R.layout.signup_popup);
+
+        //region Cancel
+        Button btnCancel = _dialog.findViewById(R.id.btnCancel_signUpPopup);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                _dialog.dismiss();
+            }
+        });
+        //endregion
+
+        //region Login
+        Button btnDone = _dialog.findViewById(R.id.btnDone_signUpPopup);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class); //replace with check login
+                startActivity(intent);
+            }
+        });
+        //endregion
+
+        //region google auth
+        Button btnGoogle = _dialog.findViewById(R.id.btnSignUpGoogle_SignUpPopup);
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class); //replace with check login
+                startActivity(intent);
+            }
+        });
+        //endregion
+
+        _dialog.show();
+
+    }//called by btnSignUp
 }
