d9a0e04571777fc2322647e1467f90f3d60c76c9
TheAidaan <56072883+TheAidaan@users.noreply.github.com>
2021-10-05 15:20:53 +0200

    can get current Location

--- Radiate/app/src/main/java/com/example/myapplication/HomeActivity.java
+++ Radiate/app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -2,36 +2,83 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class HomeActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    GoogleMap map;
+    Location _currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    static final int REQUEST_CODE = 101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_homepage);
-        mapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        FetchLastLocation();
+
+    }
+
+    void FetchLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+                return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location !=null){
+                    _currentLocation = location;
+                    Toast.makeText(getApplicationContext(),_currentLocation.getLatitude() + " " + _currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    SupportMapFragment supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_homepage);
+                    supportMapFragment.getMapAsync(HomeActivity.this::onMapReady);
+                }
+            }
+        });
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        map = googleMap;
 
-        LatLng constA = new LatLng(-34.003579, 18.443620);
-        map.addMarker(new MarkerOptions().position(constA).title("Constansia"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(constA));
+        LatLng LatLng = new LatLng(_currentLocation.getLatitude(), _currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(LatLng).title("here");
+        googleMap.addMarker(new MarkerOptions().position(LatLng).title("Constansia"));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(LatLng));
+        googleMap.addMarker(markerOptions);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+                   case REQUEST_CODE:
+                       if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                          FetchLastLocation();
+                       break;
+               }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
     }
 }
