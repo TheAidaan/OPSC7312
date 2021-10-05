95ecda4ba6db42c776182cde1e8cb800a8fdc578
TheAidaan <56072883+TheAidaan@users.noreply.github.com>
2021-10-05 20:38:14 +0200

    shows current location

--- Radiate/app/src/main/java/com/example/myapplication/HomeActivity.java
+++ Radiate/app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,7 +1,6 @@
 package com.example.myapplication;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
@@ -9,25 +8,25 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 public class HomeActivity extends FragmentActivity implements OnMapReadyCallback {
-
+Boolean _showCurrentLocation;
     Location _currentLocation;
-    FusedLocationProviderClient fusedLocationProviderClient;
+    FusedLocationProviderClient _fusedLocationProviderClient;
     static final int REQUEST_CODE = 101;
 
     @Override
@@ -35,24 +34,35 @@ public class HomeActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        FetchLastLocation();
+
+        ImageButton btnCurrentLocation  = findViewById(R.id.btn_CurrentLocation_Home);
+        btnCurrentLocation.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ShowCurrentLocation();
+            }
+        });
+
 
     }
 
+    void ShowCurrentLocation(){
+        _showCurrentLocation = true;
+        _fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        FetchLastLocation();
+    }
     void FetchLastLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
                 return;
         }
-        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        Task<Location> task = _fusedLocationProviderClient.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
                 if (location !=null){
                     _currentLocation = location;
                     Toast.makeText(getApplicationContext(),_currentLocation.getLatitude() + " " + _currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
-                    SupportMapFragment supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_homepage);
+                    SupportMapFragment supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_Home);
                     supportMapFragment.getMapAsync(HomeActivity.this::onMapReady);
                 }
             }
@@ -61,12 +71,15 @@ public class HomeActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng LatLng;
+        if (_showCurrentLocation)
+            LatLng = new LatLng(_currentLocation.getLatitude(), _currentLocation.getLongitude());
+        else
+            LatLng = new LatLng(-33.91772,18.4159913);
 
-        LatLng LatLng = new LatLng(_currentLocation.getLatitude(), _currentLocation.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions().position(LatLng).title("here");
-        googleMap.addMarker(new MarkerOptions().position(LatLng).title("Constansia"));
+        //MarkerOptions markerOptions = new MarkerOptions().position(LatLng).title("here");
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(LatLng));
-        googleMap.addMarker(markerOptions);
+        //googleMap.addMarker(markerOptions);
     }
 
     @Override
@@ -74,7 +87,7 @@ public class HomeActivity extends FragmentActivity implements OnMapReadyCallback
 
         switch (requestCode) {
                    case REQUEST_CODE:
-                       if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                       if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && _showCurrentLocation)
                           FetchLastLocation();
                        break;
                }
