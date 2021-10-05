d7d9290b9cb3afa3b76237135640acbb2c49d459
TheAidaan <56072883+TheAidaan@users.noreply.github.com>
2021-10-05 11:34:40 +0200

    Linked google maps

--- Radiate/app/src/main/java/com/example/myapplication/HomeActivity.java
+++ Radiate/app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,14 +1,37 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class HomeActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_homepage);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng constA = new LatLng(-34.003579, 18.443620);
+        map.addMarker(new MarkerOptions().position(constA).title("Constansia"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(constA));
     }
 }
