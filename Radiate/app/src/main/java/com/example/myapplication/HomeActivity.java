52d93322e9fd3c931ae92d5bcab48cd069cf743d
TheAidaan <56072883+TheAidaan@users.noreply.github.com>
2021-10-06 09:23:28 +0200

    struggling with search
    improved current location

--- Radiate/app/src/main/java/com/example/myapplication/HomeActivity.java
+++ Radiate/app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -6,10 +6,13 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -23,19 +26,67 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends FragmentActivity implements OnMapReadyCallback {
-Boolean _showCurrentLocation;
-    Location _currentLocation;
+    GoogleMap _map;
+    SupportMapFragment _mapFragment;
+
     FusedLocationProviderClient _fusedLocationProviderClient;
     static final int REQUEST_CODE = 101;
+    Location _currentLocation;
+
+    SearchView _searchView;
+    Address _address;
+
+    Boolean  _showCurrentLocation = false;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        _mapFragment  = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_home);
+        _mapFragment.getMapAsync(this::onMapReady);
 
-        ImageButton btnCurrentLocation  = findViewById(R.id.btn_CurrentLocation_Home);
+        /*_searchView  = findViewById(R.id.svSearch_home);
+        _searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = _searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if(location != null || !location.equals("")){
+                    Geocoder geocoder = new Geocoder(HomeActivity.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location,2);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                    _map.addMarker(new MarkerOptions().position(latLng).title(location));
+                    _map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });*/
+
+
+
+
+        ImageButton btnCurrentLocation  = findViewById(R.id.btn_CurrentLocation_home);
         btnCurrentLocation.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 ShowCurrentLocation();
@@ -45,12 +96,14 @@ Boolean _showCurrentLocation;
 
     }
 
+//region Current Location
     void ShowCurrentLocation(){
-        _showCurrentLocation = true;
         _fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         FetchLastLocation();
+
     }
     void FetchLastLocation() {
+        _showCurrentLocation = true;
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
                 return;
@@ -61,37 +114,36 @@ Boolean _showCurrentLocation;
             public void onSuccess(Location location) {
                 if (location !=null){
                     _currentLocation = location;
-                    Toast.makeText(getApplicationContext(),_currentLocation.getLatitude() + " " + _currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
-                    SupportMapFragment supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_Home);
+                    Toast.makeText(getApplicationContext(),location.getLatitude() + " " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+                    SupportMapFragment supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_home);
                     supportMapFragment.getMapAsync(HomeActivity.this::onMapReady);
+                    _map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()),10));
+                    _showCurrentLocation = false;
                 }
             }
         });
     }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        LatLng LatLng;
-        if (_showCurrentLocation)
-            LatLng = new LatLng(_currentLocation.getLatitude(), _currentLocation.getLongitude());
-        else
-            LatLng = new LatLng(-33.91772,18.4159913);
-
-        //MarkerOptions markerOptions = new MarkerOptions().position(LatLng).title("here");
-        googleMap.animateCamera(CameraUpdateFactory.newLatLng(LatLng));
-        //googleMap.addMarker(markerOptions);
-    }
-
-    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
         switch (requestCode) {
-                   case REQUEST_CODE:
-                       if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && _showCurrentLocation)
-                          FetchLastLocation();
-                       break;
-               }
+            case REQUEST_CODE:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && _showCurrentLocation)
+                    FetchLastLocation();
+                break;
+        }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
     }
+
+    //endregion Current Location
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        _map = googleMap;
+    }
+
+
 }
